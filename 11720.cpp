194e56898c2f905aff68931801d15ab688af724a
leeeha <jxlhe46@seoultech.ac.kr>
2021-09-30 00:36:12 +0900

    숫자의 합

--- 11720.cpp
+++ 11720.cpp
@@ -10,7 +10,7 @@ int main()
 
 	int sum = 0;
 	for (int i = 0; i < n; i++) {
-		// char형은 딱 한 문자씩만 입력 받는다!!!
+		// char형은 딱 한 문자씩만 입력 받는다!
 		cin >> arr[i];
 		sum += arr[i] - 48; // char형 '0' == 십진수 48
 	}
