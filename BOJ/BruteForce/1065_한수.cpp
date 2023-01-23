e119926fea93efa39e1fe52c65228f6eb5fe52fe
leeeha <jxlhe46@seoultech.ac.kr>
2023-01-23 13:40:28 +0900

    2023-01-23

---
+++ BOJ/BruteForce/1065_한수.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int hansoo(int num){
+	if(num < 100) return num; 
+
+	int cnt = 99; 
+	for(int i = 100; i <= num; i++){
+		int a, b, c; 
+		a = i / 100; 
+		b = i % 100 / 10; 
+		c = i % 10; 
+		
+		if((a - b) == (b - c)) {
+			cnt++; 
+		}
+	}
+
+	return cnt; 
+}
+
+int main() 
+{
+	int n; 
+	cin >> n; 
+
+	int result = hansoo(n);
+	cout << result; 
+	
+	return 0; 
+}
