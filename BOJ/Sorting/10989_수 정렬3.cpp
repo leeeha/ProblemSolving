ccbe65478bf7ea8d2a0db05e261e60587c7e0e8b
leeeha <jxlhe46@seoultech.ac.kr>
2023-02-04 11:32:27 +0900

    2023-02-04

--- BOJ/Sorting/10989_수 정렬3.cpp
+++ BOJ/Sorting/10989_수 정렬3.cpp
@@ -1,30 +1,34 @@
-// 10989번. 수 정렬3 (계수 정렬)
-// 계속 출력 초과 오류 발생,,,,
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#define MAX 10001
 using namespace std;
 
 int main()
-{
-    int n;
-    scanf("%d", &n);
+{	
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-    int count[10'000] = { 0 };
+	int n; 
+	cin >> n;
 
-    int tmp;
-    for (int i = 0; i < n; i++) {
-        // 입력: 10,000보다 작거나 같은 자연수 -> 카운팅 배열의 인덱스
-        scanf("%d", &tmp);
+	int count[MAX] {0};
 
-        // 입력값 자체를 index로 하고 (범위: 1 ~ 10000)
-        // 값의 개수를 value으로 하는 count 배열
-        count[tmp]++;
-    }
+	for(int i = 0; i < n; i++){
+		int x; 
+		cin >> x; 
 
-    // 각 숫자가 입력된 횟수만큼 출력하기
-    for (int val = 1; val <= 10'000; val++) {
-        for (int j = 0; j < count[val]; j++)
-            printf("%d\n", val);
-    }
+		// x가 입력된 횟수 카운팅 
+		count[x] += 1; 
+	}
+
+	// i를 j번 출력 
+	for(int i = 1; i < MAX; i++){
+		for(int j = 0; j < count[i]; j++){
+			cout << i << "\n"; 
+		}
+	}
 
     return 0;
 }
