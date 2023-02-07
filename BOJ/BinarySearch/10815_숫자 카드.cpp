1a31bbc8061f465e82899bc8654e5fb005b5636d
leeeha <jxlhe46@seoultech.ac.kr>
2023-02-07 14:36:59 +0900

    2023-02-07

---
+++ BOJ/BinarySearch/10815_숫자 카드.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <string>
+using namespace std;
+
+int main()
+{	
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n; 
+	cin >> n; // 최대 50만 
+
+	vector<int> arr; 
+	for(int i = 0; i < n; i++){
+		int val; 
+		cin >> val; 
+		arr.push_back(val);
+	}
+
+	// 이진탐색은 정렬된 배열에서만 사용 가능 
+	sort(arr.begin(), arr.end()); 
+
+	int m; 
+	cin >> m; 
+
+    // O(MlogN) 
+	for(int i = 0; i < m; i++){
+		int key;  
+		cin >> key; 
+
+		int start = 0, end = arr.size() - 1; 
+		bool searched = false; 
+		
+		while(start <= end){
+			int mid = (start + end)/ 2; 
+			if(arr[mid] == key){
+				searched = true; 
+				break; 
+			}else if(arr[mid] < key){ 
+				start = mid + 1; 
+			}else{ 
+				end = mid - 1; 
+			}
+		}
+
+		if(searched) cout << "1 ";
+		else cout << "0 "; 
+	}
+
+    return 0;
+}
