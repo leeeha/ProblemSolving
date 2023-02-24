a5702278396cc5a3f9a4b1c7ef0b2cbe062574b0
leeeha <jxlhe46@seoultech.ac.kr>
2023-02-24 20:40:36 +0900

    2023-02-24

--- BOJ/BackTracking/14889_스타트와 링크.cpp
+++ BOJ/BackTracking/14889_스타트와 링크.cpp
@@ -4,48 +4,61 @@
 using namespace std;
 
 const int MAX = 20;
-bool selected[MAX]; // false로 초기화 
-int arr[MAX][MAX];  // 0으로 초기화 
-int n, minVal = 1e9;
+bool selected[MAX]; 
+int arr[MAX][MAX];  
+int n; 
+int minVal = 1e9; 
 
-void dfs(int idx, int cnt){
-	vector<int> start; // 스타트 팀원의 인덱스 값
-	vector<int> link;  // 링크 팀원의 인덱스 값 
-
-	int startScore = 0;
-	int linkScore = 0;
-
-	if(cnt == n/2){ // n명 중에 n/2명 선택되면
-		for(int i = 0; i < n; i++){ 
-			if(selected[i]){ 
-				// 선택된 사람은 start 팀으로 (그 사람의 인덱스 저장) 
-				start.push_back(i); 
-			}else{ 
-				link.push_back(i); 
-			}
+void input(){
+	cin >> n;
+	
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			cin >> arr[i][j];
 		}
+	}
+}
 
-		// 각 팀의 능력치 합 계산 
-		for(int i = 0; i < n/2; i++){
-			for(int j = 0; j < n/2; j++){
-				startScore += arr[start[i]][start[j]];
-				linkScore += arr[link[i]][link[j]];
-			} 
+void updateMinVal() {
+	vector<int> start, link; 
+	int g1 = 0, g2 = 0; 
+
+	for(int i = 0; i < n; i++){ 
+		if(selected[i]){ 
+			start.push_back(i); 
+		}else{ 
+			link.push_back(i); 
 		}
+	}
 
-		// 최소 차이 구하기 
-		if(abs(startScore - linkScore) < minVal){
-			minVal = abs(startScore - linkScore);
-		}
+	// 각 팀의 능력치 계산 
+	// 각 팀원들의 번호에 맞는 행과 열을 뽑아서 
+	// 그 2차원 배열의 모든 원소들의 합을 구하면 된다. 
+	for(int i = 0; i < n/2; i++){ 
+		for(int j = 0; j < n/2; j++){  
+			g1 += arr[start[i]][start[j]]; // 0~2: 00 01 02 10 11 12 20 21 22 
+			g2 += arr[link[i]][link[j]];   // 3~5: 33 34 35 43 44 45 53 54 55 
+		} 
+	}
 
+	// 최소 차이 구하기 
+	int gap = abs(g1 - g2); 
+	if(gap < minVal){ 
+		minVal = gap; 
+	}
+}
+
+void dfs(int cnt, int idx){ // 뽑은 개수, 탐색을 진행할 인덱스  
+	if(cnt == n/2){ 
+		updateMinVal(); // 최솟값 갱신 
 		return; 
 	}
 
-	// n/2명 선택될 때까지 재귀 호출 반복 
-	for(int i = idx; i < n; i++){
+	// n명 중에 n/2명을 선택하는 조합 구하기 (i는 idx부터 시작) 
+	for(int i = idx; i < n; i++){ 
 		if(!selected[i]){ 
 			selected[i] = true; 
-			dfs(i, cnt + 1); 
+			dfs(cnt + 1, i); 
 			selected[i] = false; 
 		}
 	}
@@ -53,17 +66,11 @@ void dfs(int idx, int cnt){
 
 int main()
 { 
-	cin >> n; // 최대 20 
-	
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n; j++){
-			cin >> arr[i][j];
-		}
-	}
+	input(); 
 
-	dfs(0, 0);
+	dfs(0, 0); 
 	
-	cout << minVal << "\n"; 
+	cout << minVal; 
 	
 	return 0;
 }
