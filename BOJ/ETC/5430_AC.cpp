dabfabbabf87a2062145620b5f326d80330ba978
leeeha <jxlhe46@seoultech.ac.kr>
2023-04-19 19:21:58 +0900

    2023-04-19

--- BOJ/ETC/5430_AC.cpp
+++ BOJ/ETC/5430_AC.cpp
@@ -1,89 +1,110 @@
-#include <iostream>
-#include <vector>
+#include <iostream> 
+#include <vector> 
 #include <string>
 #include <deque>
 using namespace std;
 
-int main()
-{
+deque<int> dq;
+
+void parseString(string arr){
+	string num = "";
+	int n = arr.size();
+	
+	for(int i = 1; i < n - 1; i++){
+		char ch = arr[i];
+		
+		if(ch != ','){
+			num += ch;
+			if(i == n - 2){
+				dq.push_back(stoi(num));
+			}
+		}else{
+			dq.push_back(stoi(num)); 
+			num = "";
+		}
+	}
+}
+
+void printComma() {
+	if(!dq.empty()){
+		cout << ","; 
+	}
+}
+
+void printForwardArray() {
+	while(!dq.empty()){
+		cout << dq.front();
+		dq.pop_front();
+		printComma(); 
+	}
+}
+
+void printBackwardArray() {
+	while(!dq.empty()){
+		cout << dq.back(); 
+		dq.pop_back();
+		printComma(); 
+	}
+}
+
+void printArray(bool forward){
+	cout << "[";
+	if(forward){
+		printForwardArray(); 
+	}else{
+		printBackwardArray(); 
+	}
+	cout << "]\n";
+}
+
+int main() {
 	ios::sync_with_stdio(0);
     cin.tie(0);
-	
+
 	int t;
 	cin >> t; 
 
-	while (t--) { // 최대 100개 
+	while (t--) { // 최대 100
 		string p;
-		cin >> p; // 배열을 변형할 문자열 (최대 길이 10만) 
-
+		cin >> p; // 최대 10만
+		
 		int n;
-		cin >> n; // 배열 원소 개수 (최대 10만개) 
+		cin >> n; // 최대 10만
+		
+		string arr;
+		cin >> arr;
 
-		string s; 
-		cin >> s; // 대괄호를 포함한 정수 배열 입력
-
-		deque<int> dq; 
-
-		string temp; // 두 자릿수 이상의 숫자를 위해 
-		for(int i = 0; i < s.size(); i++){
-			if(s[i] == '['){
-				continue;
-			}
-			else if('0' <= s[i] && s[i] <= '9'){
-				temp += s[i];
-			}
-			else if(s[i] == ',' || s[i] == ']'){
-				if(!temp.empty()){
-					dq.push_back(stoi(temp));
-					temp.clear();
-				}
-			}
-		}
-
-		bool print = true; // error가 아닐 경우 true
-		bool left = true;  // 뒤집어져 있지 않으면 true
+		parseString(arr);
+	
+		bool isError = false;
+		bool forward = true; // 정방향이면 true 
+		
 		for(int i = 0; i < p.size(); i++){
 			if(p[i] == 'R'){
-				left = !left;
+				// 방향 전환 
+				forward = !forward;
 			}else{
+				// 비어있는데 삭제하려고 하면 에러 
 				if(dq.empty()){
-					print = false;
+					isError = true;
 					cout << "error\n";
 					break;
+				}
+
+				// 현재 방향에 따라 앞이나 뒤의 원소 삭제 
+				if(forward){
+					dq.pop_front();
 				}else{
-					if(left){ // 뒤집어져 있지 않으면 
-						dq.pop_front(); // 왼쪽 원소 제거 
-					}else{ // 뒤집어져 있으면 
-						dq.pop_back(); // 오른쪽 원소 제거 
-					}
+					dq.pop_back();
 				}
 			}
 		}
 
-		if(print){    // error가 아니고 
-			if(left){ // 뒤집어져 있지 않으면 
-				cout << "[";
-				while(!dq.empty()){
-					cout << dq.front();
-					dq.pop_front(); // 앞에서 pop 
-					if(!dq.empty()){
-						cout << ","; 
-					}
-				}
-				cout << "]\n";
-				}else{ // 뒤집어져 있으면 
-					cout << "[";
-					while(!dq.empty()){
-						cout << dq.back(); 
-						dq.pop_back(); // 뒤에서 pop
-						if(!dq.empty()){
-							cout << ",";
-						}
-					}
-					cout << "]\n";
-				}
-		}		
+		if(!isError){
+			// 현재 방향에 따라 배열 출력 
+			printArray(forward);
+		}
 	}
-
+	
 	return 0;
 }
--- BOJ/Queue/1966_프린터 큐.cpp
+++ BOJ/Queue/1966_프린터 큐.cpp
@@ -2,59 +2,57 @@
 #include<queue>
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-    // 테스트 케이스의 수
-    int test;
-    cin >> test;
+    int t;
+    cin >> t;
 
-    for (int i = 0; i < test; i++) {
-        // n: 문서의 수
-        // m: 몇번째로 인쇄되었는지 궁금한 문서의 현재 큐에서의 위치
+	while(t--){
         int n, m;
         cin >> n >> m;
-
+        
         // 인덱스가 m인 문서가 몇번째로 인쇄되었는지 카운팅하는 변수
-        int cnt = 0; // 각 테스트 케이스마다 0으로 초기화!!!
+        int cnt = 0;
 
         queue<pair<int, int>> q;
-        priority_queue<int> pq;
+        priority_queue<int> pq; // 기본적으로 최대 힙 
 
-        int ipt; // 각 문서의 중요도
-        for (int j = 0; j < n; j++) {
-            cin >> ipt;
-
-            // 각 문서의 인덱스와 중요도를 묶어서 큐에 push
-            q.push({ j, ipt });
-
-            // 우선순위 큐는 중요도에 따라 내림차순 정렬
-            pq.push(ipt);
+        for (int i = 0; i < n; i++){
+			int x; 
+			cin >> x; 
+			
+            q.push({ i, x }); // 인덱스와 중요도 
+            pq.push(x); // 중요도가 높은 순으로 
         }
 
-        while (!q.empty()) {
+        while (!q.empty()){
             // 큐의 front 원소를 꺼내서
             int idx = q.front().first;
-            int val = q.front().second;
+            int priority = q.front().second;
             q.pop();
 
             // 우선순위 큐의 top과 중요도가 일치하면
-            if (pq.top() == val) {
+            if (pq.top() == priority){
                 pq.pop();
                 cnt++; // 문서 하나 인쇄할 때마다 증가
 
                 // 현재 문서의 인덱스가 m인 경우
-                if (idx == m) {
+                if (idx == m){
                     // 몇번째로 인쇄되었는지 출력
-                    cout << cnt << '\n';
+                    cout << cnt << '\n'; 
                     break; // 그 다음 테스트 케이스로 넘어가기
                 }
             }
-            else { // 중요도가 높지 않으면 큐의 rear에 다시 push
-                q.push({ idx, val });
+            else { 
+				// 중요도가 높지 않으면 큐의 rear에 다시 push
+                q.push({ idx, priority });
             }
         }
+
+        // 각 테스트 케이스마다 q, pq를 새로 생성하기 때문에 
+        // 컨테이너 초기화는 필요하지 않다.  
     }
 
     return 0;
