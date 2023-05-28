373f71a39bb690af55f8be404a80bf884c61189d
leeeha <jxlhe46@seoultech.ac.kr>
2023-05-28 17:27:07 +0900

    2023-05-28

--- BOJ/Greedy/1789_수들의 합.cpp
+++ BOJ/Greedy/1789_수들의 합.cpp
@@ -1,30 +1,30 @@
-// https://chanhuiseok.github.io/posts/baek-31/
-// 변수 타입을 ll으로 선언해야 시간초과가 뜨지 않는다. 
-
-#include <iostream>
-using namespace std; 
+#include <iostream> 
+#include <vector> 
+#include <string>
+#include <algorithm>
+using namespace std;
 
 int main() {
 	ios::sync_with_stdio(0);
-	cin.tie(0);
+    cin.tie(0);
 
-	long long S;
-	cin >> S;
+	long long S; 
+	cin >> S; 
 
-	long long sum = 0, num = 1; 
-	int cnt = 0; 
+	long long sum = 0; // 데이터 범위 잘 확인하기!!! 
+	int n = 1; // 더하는 수 자체 
+	int cnt = 0; // 더한 개수 
+	
+	while(sum < S){ 
+		sum += n; 
 
-	while(true) {
-		sum += num; 
-		cnt++;
-		if(sum > S){
-			cnt--;
-			break;
-		}
-		num++;
+		if(sum > S) break; 
+		
+		cnt++; 
+		n += 1; 
 	}
 
 	cout << cnt; 
 	
-	return 0; 
+	return 0;
 }
