c8c19f8a52d3fb1c3e3c1fb475d6fe10e1e0d857
leeeha <jxlhe46@seoultech.ac.kr>
2023-05-31 17:57:03 +0900

    2023-05-31

--- BOJ/DFS_BFS/7576_토마토.cpp
+++ BOJ/DFS_BFS/7576_토마토.cpp
@@ -1,55 +1,56 @@
-#include <iostream>
-#include <vector>
-#include <queue> 
+#include <iostream> 
+#include <vector> 
+#include <string>
 #include <algorithm>
-#include <utility> 
-#define MAX 1001 
+#include <cstring> 
+#include <queue> 
 using namespace std;
-typedef pair<int, int> pii; 
 
+const int MAX = 1001; 
 int m, n; 
-int graph[MAX][MAX]; // 0으로 초기화 
-queue<pii> q; // 1의 위치 저장 
+int graph[MAX][MAX];
+queue<pair<int, int>> q; // 1의 좌표  
 
-int dy[] = {-1, 1, 0, 0}; // 행 
-int dx[] = {0, 0, -1, 1}; // 열 
-int result = 0; 
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
 
-void bfs(){
+void bfs() {
 	while(!q.empty()){
-		int y = q.front().first;  // 행 
-		int x = q.front().second; // 열 
-		q.pop();
+		// 1의 위치를 하나씩 꺼내서 
+		int x = q.front().first;
+		int y = q.front().second;
+		q.pop(); 
 
+		// 상하좌우에 0이 있는지 탐색한다. 
 		for(int i = 0; i < 4; i++){
-			int ny = y + dy[i];
 			int nx = x + dx[i];
+			int ny = y + dy[i];
 			
-			if(ny >= 0 && ny < n && nx >= 0 && nx < m){
-				if(graph[ny][nx] == 0){
-					// 해당 칸에 도달할 때까지 걸린 시간 저장 
-					graph[ny][nx] = graph[y][x] + 1; 
-					
-					// 0을 1로 물들이기 (큐에 저장)
-					q.push({ny, nx});
-				}
+			if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue; 
+
+			// 0을 발견할 때까지 걸린 시간을 해당 위치에 저장 
+			if(graph[nx][ny] == 0){ 
+				graph[nx][ny] = graph[x][y] + 1; 
+
+				// 0에서 1로 물든 토마토의 위치를 큐에 저장 
+				q.push({nx, ny}); 
 			}
 		}
 	}
 }
 
-int main(){
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
+int main() {
+	ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	cin >> m >> n; // m x n 
+	cin >> m >> n; // n x m 
 
 	for(int i = 0; i < n; i++){
-		for(int j = 0; j < m; j++){
+		for(int j = 0; j < m; j++){ 
 			cin >> graph[i][j]; 
 
-			// 1의 위치를 큐에 저장 
-			if(graph[i][j] == 1){ 
+			// 1의 위치 저장 
+			if(graph[i][j] == 1){
 				q.push({i, j}); 
 			}
 		}
@@ -57,22 +58,22 @@ int main(){
 
 	bfs(); 
 
+	int ans = 0; 
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < m; j++){
 			if(graph[i][j] == 0){
-				printf("-1\n");
+				cout << "-1\n";
 				return 0; 
 			}
 
-			if(result < graph[i][j]){
-				result = graph[i][j]; // 최댓값 저장 
+			if(ans < graph[i][j]){
+				ans = graph[i][j]; 
 			}
 		}
 	}
 
-	// 첫째날에 익게 된 0의 위치에 
-	// 1+1이 저장되니까 출력할 때는 -1을 해줘야 한다. 
-	cout << result - 1; 
+	// 최댓값에서 1을 빼면 정답 
+	cout << ans - 1; 
 	
-	return 0; 
+	return 0;
 }
