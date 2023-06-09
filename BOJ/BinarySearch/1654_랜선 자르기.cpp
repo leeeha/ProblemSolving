5757d40af35cbee7a2c83b8fa9c1a161e3a0360f
leeeha <jxlhe46@seoultech.ac.kr>
2023-06-09 15:42:24 +0900

    2023-06-09

--- BOJ/BinarySearch/1654_랜선 자르기.cpp
+++ BOJ/BinarySearch/1654_랜선 자르기.cpp
@@ -1,61 +1,40 @@
-#include <iostream>
-#include <algorithm> 
+#include <iostream> 
 #include <vector>
-typedef long long ll; 
-using namespace std; 
+#include <algorithm>
+using namespace std;
+typedef long long ll;
 
-int k, n; // 최대 1만, 100만 
-ll arr[10001]; 
-ll answer = 0; 
+int k, n;
+const int MAX = 10001; 
+ll lines[MAX];
 
-// x의 길이가 너무 크면, 개수가 n개를 안 넘고 
-// x의 길이가 너무 작으면, 개수가 n개를 훌쩍 넘는다. 
-bool decision(int x){ 
+bool decision(ll length){
 	int sum = 0;
-	for(int i = 0; i < k; i++){
-		sum += arr[i] / x; 
+	for(int i = 0; i < k; i++){ 
+		sum += lines[i] / length; 
 	}
-
-	if(sum >= n){ 
-		return true;
-	}else{
-		return false; 
-	}
+	return sum >= n; 
 }
 
-int main()
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
+int main() {
+	ios::sync_with_stdio(0);
+    cin.tie(0);
 
 	cin >> k >> n; 
 
-	for(int i = 0; i < k; i++){ 
-		cin >> arr[i];
+	for(int i = 0; i < k; i++){
+		cin >> lines[i]; 
 	}
 
-	ll left = 1; 
-	ll right = *max_element(arr, arr + k); 
-
-	// 이진 탐색 진행 
+	// 결정 문제를 만족시키는 x의 최댓값을 구하자. 
+	ll left = 0, right = 3e9;
 	while(left <= right){
-		ll mid = (left + right) / 2;
-
-		// mid 길이로 자른 개수가 n개 이상이면
-		if(decision(mid)){
-			// x의 최댓값 갱신
-			answer = max(answer, mid);
-
-			// x의 길이를 더 늘려서, 개수를 줄인다.
-			left = mid + 1; 
-		}else{ 
-			// 자른 개수가 n개 미만이면 
-			// x의 길이를 더 줄여서, 개수를 늘린다. 
-			right = mid - 1;
-		}
+		ll mid = (left + right) / 2; 
+		if(decision(mid)) left = mid + 1; 
+		else right = mid - 1;
 	}
 
-	cout << answer; 
+	cout << right; 
 
 	return 0;
 }
