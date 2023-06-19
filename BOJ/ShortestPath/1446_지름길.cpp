b69c2d401ebaaa5829c8633e1f5dcab76defe1d5
leeeha <jxlhe46@seoultech.ac.kr>
2023-06-19 22:39:28 +0900

    2023-06-19

--- BOJ/ShortestPath/1446_지름길.cpp
+++ BOJ/ShortestPath/1446_지름길.cpp
@@ -1,63 +1,70 @@
 #include <iostream>
+#include <queue>
 #include <vector>
-#include <algorithm>
-#include <string> 
-#define INF 1e9
-#define MAX 10001
+#define MAX 10001 
 using namespace std;
 
+// 지름길의 개수, 고속도로 길이 
 int N, D; 
-vector<pair<int, int>> graph[MAX]; // 지름길 정보 저장 
-int dist[MAX]; // 특정 지점까지의 최소 운전 거리를 저장하는 테이블 
 
-void input() {
-	cin >> N >> D;
+// 지름길의 위치, 거리를 저장하는 배열 
+vector<pair<int, int>> path[MAX]; 
 
-	// i 위치까지 가는 최소 운전 거리 초기화 
+// 특정 위치까지 가는 최단 거리를 저장하는 테이블 
+int dist[MAX];
+
+void input(){
+	cin >> N >> D; 
+
+	// i 위치까지 가는 최단 거리 초기화 
 	for(int i = 0; i <= D; i++){
-		dist[i] = i; 
+		dist[i] = i; 	
 	}
 
-	// 지름길 정보 저장 
 	for(int i = 0; i < N; i++){
+		// 지름길의 시작점, 도착점, 길이 
 		int start, end, length; 
-		cin >> start >> end >> length;
+		cin >> start >> end >> length; 
 
-		if(end - start <= length) continue; 
 		if(end > D) continue; 
-		
-		graph[start].push_back({end, length}); 
+		if(end - start <= length) continue; 
+
+		path[start].push_back({end, length});
 	}
 }
 
 int main(void) {
-    ios::sync_with_stdio(0);
+	ios::sync_with_stdio(0);
     cin.tie(0);
 
 	input(); 
 
-	int before = -1;
-	for(int i = 0; i <= D; i++){ 
-		if(i != 0) before = dist[i - 1]; 
+	int before; 
+	for(int i = 0; i <= D; i++){
+		if(i == 0) before = -1; 
+		else before = dist[i - 1]; 
 
-		// 지름길을 따라가는 경우: dist[i]
-		// 일반 고속도로를 따라가는 경우: dist[i - 1] + 1 
-		// -> i 위치까지 가는 최소 운전 거리 갱신 
-		dist[i] = min(dist[i], before + 1); 
+		// 지름길 반영한 최단 거리: dist[i] 
+		// 일반 고속도로 이용한 거리: dist[i - 1] + 1 
+		dist[i] = min(dist[i], before + 1);
 
-		// i 위치에 지름길이 있는 경우 
-		for(auto edge: graph[i]){ 
+		// i 위치에서 출발하는 지름길이 없다면 무시 
+		if(path[i].empty()) continue; 
+
+		// i 위치에서 출발하는 지름길이 있다면 
+		for(auto edge: path[i]){ 
 			int end = edge.first; 
-			int length = edge.second; 
+			int length = edge.second;
 
-			// 지름길을 이용하는 게 더 짧으면 테이블 갱신 
-			if(dist[end] > dist[i] + length){ 
+			// 최단 거리 테이블 갱신 
+			if(dist[end] > dist[i] + length){
 				dist[end] = dist[i] + length; 
 			}
 		}
 	}
 
-	cout << dist[D]; 
-	
-	return 0; 
+	// D까지 가는 최단 거리 출력 
+	cout << dist[D] << endl; 
+
+    return 0;
 }
