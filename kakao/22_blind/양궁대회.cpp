f739d579836db23c692312086b8d4718d969f17e
leeeha <jxlhe46@gmail.com>
2024-06-06 21:24:48 +0900

    [comment] 양궁대회 코드 주석 추가

--- kakao/22_blind/양궁대회.cpp
+++ kakao/22_blind/양궁대회.cpp
@@ -33,47 +33,55 @@ int calcScoreGap(){
 
 bool checkLowerCase() {
     for(int i = MAX - 1; i >= 0; i--){
-        if(ryan[i] > ans[i]) return true; 
-        else if(ryan[i] < ans[i]) return false;
+        if(ryan[i] != ans[i]) 
+            return ryan[i] > ans[i];
     }
-    return false; 
+    return false;
 }
 
 void updateResult(int chance, int idx){
     int scoreGap = calcScoreGap();
     
+    // 라이언이 이길 수 있는 경우 
     if(scoreGap != -1){
-        // 기회가 남았는데 idx == MAX 인 경우
-        // 0번 과녁에 남은 화살을 모두 쏜다. 
+        // 남은 모든 화살을 0점에 맞힌다. 
         if(chance > 0) ryan[idx - 1] = chance; 
         
-        // 최대 점수 차이 && 가장 낮은 점수를 더 많이 맞힌 경우 
+        // 최대 점수 차이를 만들 수 있으면서 
+        // 가장 낮은 점수에 더 많이 맞힌 경우 
         if(scoreGap == maxScoreGap && checkLowerCase()){
-            ans = ryan;
+            ans = ryan; // 방법 갱신 
         }else if(scoreGap > maxScoreGap){
-        	// 최대 점수 차이 갱신 
-            maxScoreGap = scoreGap;
-            ans = ryan;
+            maxScoreGap = scoreGap; // 최대 점수 차이 갱신 
+            ans = ryan; // 방법 갱신 
         }
         
+        // 0점 과녁에 맞힌 화살 개수 초기화 
         ryan[idx - 1] = 0;
     }
 }
 
 void dfs(int chance, int idx){
+    // 10~0점까지 화살 개수 분배를 마쳤거나 
+	// n개의 화살을 전부 사용했으면 
     if(idx == MAX || chance == 0){
+    	// 결과 갱신 
         updateResult(chance, idx);
         return; 
     }
     
     if(chance > apeach[idx]){
-        // 어피치보다 한발 더 맞히기
+        // i) 어피치보다 한발 더 맞히는 경우 
         ryan[idx] = apeach[idx] + 1;  
+        
+        // 재귀 호출 
         dfs(chance - ryan[idx], idx + 1);
+        
+        // 다음 경우의 수를 위한 상태 복구 
         ryan[idx] = 0;
     }
     
-    // 화살을 쏘지 않고 다음으로 패스
+    // ii) 화살을 쏘지 않고 다음으로 넘어가는 경우 
     dfs(chance, idx + 1);
 }
 
