5aede07cccdb665545badad6f29e52c92ecf7a46
leeeha <jxlhe46@gmail.com>
2024-10-04 16:02:26 +0900

    [add] 문자열 문제 추가

---
+++ Programmers/kotlin/문자열 나누기.kt
@@ -0,0 +1,33 @@
+import java.util.*
+import kotlin.math.*
+
+class Solution {
+    fun solution(s: String): Int {
+        var answer = 0
+        val counting = arrayOf(0, 0)
+        var x = s[0]
+        var splitted = false
+        
+        s.forEachIndexed { idx, ch ->
+            if(splitted){
+                x = ch // 첫문자 갱신 
+                splitted = false // 분리 여부 갱신 
+            }
+            
+            // 첫문자와 같은지 검사 
+            if(x == ch){
+                counting[0]++
+            }else{
+                counting[1]++
+            }
+            
+            // 개수 비교 
+            if(counting[0] == counting[1] || idx == s.lastIndex){
+                answer++
+                splitted = true 
+            }
+        }
+        
+        return answer 
+    }
+}
